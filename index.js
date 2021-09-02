3ba2125f7e04e561edd56d3b7ed2627fba66b733
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-01 22:24:28 -0700

    created file and added questions

---
+++ index.js
@@ -0,0 +1,34 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: 
+    message:
+    name:
+}, {
+    type: 
+    message:
+    name:
+},{
+    type: 
+    message:
+    name:
+},{
+    type: 
+    message:
+    name:
+}];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
