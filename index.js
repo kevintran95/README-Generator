13b41f1307e31bf692173e649c48051192532520
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-02 21:12:21 -0700

    constructed questions array

--- index.js
+++ index.js
@@ -2,27 +2,48 @@
 const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [{
+const questions = [
+    {
     type: "input",
     message: "What is the title of the project?",
     name: "Title"
+}, 
+{
+    type: "input",
+    message:"Write a brief description about your project:",
+    name: "description"
 }, {
-    type: 
-    message:
-    name:
-}, {
-    type: 
-    message:
-    name:
-},{
-    type: 
-    message:
-    name:
-},{
-    type: 
-    message:
-    name:
-}];
+
+    type: "input",
+    message:'Please enter installation instructions',
+    name:'installation'
+},
+{
+    type: 'input',
+    message:'Please enter usage information',
+    name:'usage'
+},
+{
+    type: 'input',
+    message:'What licenses will you be using',
+    name:'license'
+},
+{
+    type:'input',
+    message:'Please enter test instructions',
+    name:'test-instructions'
+},
+{
+    type:'input',
+    message:'Please enter your github URL',
+    name:'githubURL'
+},
+{
+    type:'input',
+    message:'Please enter your contact email',
+    name:'contact'
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
